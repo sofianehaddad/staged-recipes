e36ee8309c38e2e4368f1dd763ebd243e6d83f35
Julien Schueller <schueller@phimeca.com>
2016-05-05 12:39:58 +0200

    Add nlopt recipe

---
+++ recipes/nlopt/bld.bat
@@ -0,0 +1,28 @@
+@echo off
+
+@rem See https://github.com/jyypma/nloptr/blob/master/INSTALL.windows
+cp "%RECIPE_DIR%\CMakeLists.txt" .
+if errorlevel 1 exit 1
+cp "%RECIPE_DIR%\config.cmake.h.in" .
+if errorlevel 1 exit 1
+
+mkdir build
+cd build
+
+set CMAKE_CONFIG="Release"
+
+if not defined CMAKE_GENERATOR EXIT 1
+
+cmake -LAH -G%CMAKE_GENERATOR%                               ^
+    -DPYTHON_EXECUTABLE="%PYTHON%"                           ^
+    -DPYTHON_INCLUDE_PATH="%PREFIX%\include"                 ^
+    -DPYTHON_LIBRARY="%PREFIX%\libs\python%PY_VER:.=%.lib"   ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ..
+
+cmake --build . --config %CMAKE_CONFIG% --target ALL_BUILD
+cmake --build . --config %CMAKE_CONFIG% --target INSTALL
+
+if errorlevel 1 exit 1
+
+
+
